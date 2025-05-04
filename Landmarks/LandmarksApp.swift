db0de147a9a34960d0a56b091f83ef4c5df34b73
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2025-05-04 14:19:14 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mitsutan on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
